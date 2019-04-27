429a546dfbe2e78e859b24ae45c0a49411b34d5a
The-Ocean <49565822+The-Ocean@users.noreply.github.com>
2019-04-27 23:32:17 +0800

    Update BubbleSort.h

--- 排序/排序/BubbleSort.h
+++ 排序/排序/BubbleSort.h
@@ -20,7 +20,9 @@ void Print5(int arr[], int size)
 //冒泡排序
 //思想：
 //9 5 2 7 4 1 3 
-//
+//1.两个数之间相互比较大小，每次将较大的值的数字放在后面
+//2.循环整个数组，一次遍历后就将最大值放在了最后
+//3.在--size,在次循环
 void Sort5(int arr[], int size)
 {
 	while (size)
